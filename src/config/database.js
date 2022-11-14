7b5bf1d63e5549e205ea23f16964256102747356
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2022-11-13 22:45:43 -0300

    .env

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 config()
 
 try {
-    const db = await mongoose.connect(process.env.MONGODB_URI)
+    const db = mongoose.connect(process.env.MONGODB_URI)
     console.log('Connected to', db.connection.name)
 } catch (error) {
     console.log(error)
